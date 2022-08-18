d336da3653801bb2920210f626d55989f57114bc
Atuti <hobsonmokaya@gmail.com>
2022-08-18 14:41:01 +0300

    added openapi documentation

--- url-shortener-clone/src/main/java/org/mokaya/resource/UrlShortenerApi.java
+++ url-shortener-clone/src/main/java/org/mokaya/resource/UrlShortenerApi.java
@@ -9,15 +9,54 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 @Path("/api")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@Tag(name = "UrlShortenerApi")
 public interface UrlShortenerApi {
     
     @GET
     @Path("/{alias}")
+    @Operation(
+        summary = "getUrlShortener",
+        description = "get url shortener from alias"
+    )
+
+    @APIResponse(
+        responseCode = "301",
+        description = "Url shortener successfully found"
+    )
+    @APIResponse(
+        responseCode = "404",
+        description = "Alias not found"
+    )
+    @APIResponse(
+        responseCode = "400",
+        description = "Alias not valid"
+    )
     Response getUrlShortener(@PathParam("alias") String alias);
 
     @POST
+    @Operation(
+        summary = "createUrlShortener",
+        description = "create a new url shortener"
+    )
+
+    @APIResponse(
+        responseCode = "200",
+        description = "Url shortener successfully created"
+    )
+    @APIResponse(
+        responseCode = "409",
+        description = "Alias already exists"
+    )
+    @APIResponse(
+        responseCode = "400",
+        description = "Input not valid"
+    )
     Response createUrlShortener(UrlShortenerDTO urlShortenerDTO);
 }
